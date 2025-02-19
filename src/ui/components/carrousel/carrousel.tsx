330af9f3835dd385768d528bf31646542a1c7193
Gustavo Vinicius <gus.tec.dev@gmail.com>
2025-02-19 18:59:52 -0300

    Organizando as mensagens dos slides em um array de objetos

--- src/ui/components/carrousel/carrousel.tsx
+++ src/ui/components/carrousel/carrousel.tsx
@@ -9,19 +9,41 @@ import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import Image from 'next/image';
 
 export default function Carrousel() {
+    const slides = [
+        {
+            'img': 'praia1.jpg',
+            'title': 'Descubra Lugares Incríveis',
+            'description': 'Explore praias paradisíacas ao redor do mundo!'
+        },
+        {
+            'img': 'praia2.jpg',
+            'title': 'Sinta a natureza',
+            'description': 'Passeie por locais com rica natureza!'
+        },
+        {
+            'img': 'praia3.jpg',
+            'title': 'Conheça as praias do nordeste',
+            'description': 'Litoral de joão pessoa - Pernambuco - Natal'
+        },
+        {
+            'img': 'praia4.jpg',
+            'title': 'Tenha novas experiências',
+            'description': 'Explore praias paradisíacas ao redor do mundo!'
+        },
+    ];
 
     return(
         <>
             <Swiper
-                modules={[Autoplay, Pagination]}
+                modules={[Autoplay]}
                 spaceBetween={30}
                 centeredSlides={true}
-                autoplay={{ delay: 2500, disableOnInteraction: false }}
+                autoplay={{ delay: 3500, disableOnInteraction: false }}
                 loop={true}
                 pagination={{ clickable: true }}
                 className="w-full max-w-5xl"
             >
-                {['praia1.jpg', 'praia2.jpg', 'praia3.jpg', 'praia4.jpg'].map((img, index) => (
+                {slides.map(({ img, title, description }, index) => (
                 <SwiperSlide key={index}>
                     <div className="relative w-full h-[500px] rounded-lg overflow-hidden">
                         <Image
@@ -31,8 +53,8 @@ export default function Carrousel() {
                             objectFit="cover"
                         />
                         <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center p-6 bg-black bg-opacity-40">
-                            <h3 className="text-3xl font-bold">Descubra Lugares Incríveis</h3>
-                            <p className="mt-2 text-lg">Explore praias paradisíacas ao redor do mundo!</p>
+                            <h3 className="text-3xl font-bold">{title}</h3>
+                            <p className="mt-2 text-lg">{description}</p>
                         </div>
                     </div>
                 </SwiperSlide>
